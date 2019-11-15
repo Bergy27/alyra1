2ba35a72b5b5c14937231423a821e6a33391cdfc
Bastien Berger <bastienberger@MacBook-Pro-de-Bastien.local>
2019-11-15 10:46:22 +0100

    exercice 1.1.2

---
+++ alyra1_1_2.py
@@ -0,0 +1,42 @@
+import sys
+from functools import reduce
+
+
+def search(bounds):
+	return round(reduce((lambda x,y : x+y), bounds)/2)
+
+def compare(solution, attempt, bounds):
+	if solution-attempt>0:
+		return {
+			"found" : False,
+			"bounds" : [attempt+1, bounds[1]]
+		}
+	elif solution-attempt<0:
+		return {
+			"found" : False,
+			"bounds" : [bounds[0], attempt-1]
+		}
+	else:
+		return {
+			"found" : True,
+			"bounds" : [bounds[0], attempt]
+		}
+
+try:
+	solution = int(sys.argv[1])
+except:
+	print("Argument must be an integer")
+	sys.exit(1)
+
+assert solution>0 and solution<=100, "Argument must be between 1 and 100"
+
+obj = {
+	"found" : False,
+	"bounds" : [1, 100] 
+}
+
+while not obj["found"]:
+	mean = search(obj["bounds"])
+	obj = compare(solution, mean, obj["bounds"])
+
+print("Bravo, la réponse était {}".format(mean))
