7d03d521d0a358e2fd9e8af849fd0214a5c59e31
Berger Bastien <bastien.berger@hesge.ch>
2020-04-26 14:33:48 +0200

    ex 4.2.2

--- alyra3/DApp/application.js
+++ alyra3/DApp/application.js
@@ -1,3 +1,83 @@
+let CONTRACT_ABI = [
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "dev",
+        "type": "bytes32"
+      }
+    ],
+    "name": "remettre",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "cred",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "dd",
+        "type": "string"
+      }
+    ],
+    "name": "produireHash",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bytes32"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "pure",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "destinataire",
+        "type": "address"
+      },
+      {
+        "name": "valeur",
+        "type": "uint256"
+      }
+    ],
+    "name": "transfer",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+]
+//let CONTRACT_ADDRESS = "0xfC3a6fE7B67679CCbACa927e4E30952F44553d55";
+let CONTRACT_ADDRESS = "0x451875bdd0e524882550ec1ce52bcc4d0ff90eae";
+
 async function createMetaMaskDapp() {
 	try {
 		// Demande à MetaMask l'autorisation de se connecter
@@ -5,8 +85,11 @@ async function createMetaMaskDapp() {
 		const address = addresses[0]
 		// Connection au noeud fourni par l'objet web3
 		const provider = new ethers.providers.Web3Provider(ethereum);
-		dapp = { address, provider };
-		//console.log(dapp)
+		let monContrat=new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, provider);
+		let monContratSigne=monContrat.connect(provider.getSigner(address.address));
+		dapp = { provider, monContrat, monContratSigne, address}
+		//dapp = { address, provider };
+
 		return dapp;
 	} catch(err) {
 		// Gestion des erreurs
@@ -21,13 +104,43 @@ async function balance(){
 		console.log("Balance: " + etherString);
 		document.getElementById("balance").innerHTML = "Balance : "+etherString;
 	});
-	dapp.provider.getBlockNumber().then((bloc) => {
-		console.log("Dernier bloc : " + bloc);
-		document.getElementById("last_bloc").innerHTML = "Dernier bloc : "+bloc;
+}
+
+async function blockNumber() {
+	if (typeof dapp === "undefined") { await createMetaMaskDapp(); }
+		dapp.provider.getBlockNumber(dapp.address).then((blockNumber) => {
+    	console.log("Dernier bloc: " + blockNumber);
+    	document.getElementById("last_bloc").innerHTML = "Dernier bloc : "+blockNumber;
 	});
-	dapp.provider.getGasPrice().then((balance) => {
-		let etherString = ethers.utils.formatEther(balance);
-		console.log("Balance: " + etherString);
-		document.getElementById("gaz").innerHTML = "Gaz : "+etherString;
+}
+
+async function gasPrice() {
+	if (typeof dapp === "undefined") { await createMetaMaskDapp(); }
+	dapp.provider.getGasPrice(dapp.address).then((gasPrice) => {
+		let etherString = ethers.utils.formatEther(gasPrice);
+		console.log("Prix du gaz: " + etherString);
+		document.getElementById("gaz").innerHTML = "Prix du gaz : "+etherString;
 	});
+}
+
+async function credibilite() {
+	if (typeof dapp === "undefined") { await createMetaMaskDapp(); }
+
+	let contratCredibilite = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, dapp.provider.getSigner());
+	let maCredibilite = await contratCredibilite.cred(dapp.address);
+	console.log(maCredibilite);
+	document.getElementById("credibilite").innerHTML = "Ma crédibilité est de : "+maCredibilite;
+}
+
+async function rendreDevoir(url) {
+	if (typeof dapp === "undefined") { await createMetaMaskDapp(); }
+	let contratCredibilite = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, dapp.provider.getSigner());
+	var hashDevoir = contratCredibilite.produireHash(url);
+	hashDevoir.then(function(hash) {
+		console.log(contratCredibilite.remettre(hash));
+		credibilite();
+	})
+	//credibilite();
+	//console.log(hashDevoir.then);
+	//remettre
 }
